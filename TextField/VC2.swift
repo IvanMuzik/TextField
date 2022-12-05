07d95c5e9a419e71b86a25a1564c73e6f9009d3f
Ivan <ivan>
2022-12-05 11:59:18 +0300

    add Registration View

---
+++ TextField/VC2.swift
@@ -0,0 +1,12 @@
+//
+//  VC2.swift
+//  TextField
+//
+//  Created by Ivan Muzalevski on 5.12.22.
+//
+
+import UIKit
+
+class VC2: NSObject {
+
+}
--- TextField/ViewController.swift
+++ TextField/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var loginTextfield: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    let login = "I"
+    let password = "1"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func enterButtonAction(_ sender: UIButton) {
+        if loginTextfield.text == login && passwordTextField.text == password {
+            
+        }
+    }
+    
 }
 
